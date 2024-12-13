f3186a2ece48973633a278d3479023cf8a650d65
hzg20 <1960941659@qq.com>
2024-12-14 00:08:32 +0800

    docs: 更新 App.vue 文件
    - 在 Vite logo 下方添加了数字序列 "123456"

--- App.vue
+++ App.vue
@@ -5,6 +5,7 @@
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
+      123456
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
