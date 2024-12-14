809dcb368f9f24c11981bea6521503d987cb6cee
hzg20 <1960941659@qq.com>
2024-12-14 19:36:33 +0800

    feat: 添加 TailwindCSS 和 Vue Router 支持
    - 在项目中集成 TailwindCSS 和 tailwindcss-primeui 插件- 添加 Vue Router 并创建基本路由配置
    - 新增 tailwind.config.js 文件进行 TailwindCSS配置
    - 在 package.json 中添加相关依赖

---
+++ router/router.ts
@@ -0,0 +1,14 @@
+import { createWebHashHistory, createRouter } from 'vue-router'
+
+
+
+const routes = [
+
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+export default router
